91a0f0931517f872b88b78c6276349f089650324
Zsy-hope <1151216974@qq.com>
2025-12-28 16:45:23 +0800

    02_点击激活实现

---
+++ src/Sku/Sku.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="goods-sku">
+    <dl v-for="item in goods.specs" :key="item.id">
+      <dt>{{ item.name }}</dt>
+      <dd>
+        <template v-for="val in item.values" :key="val.name">
+          <!-- 图片类型规格 -->
+          <img :class="{ selected: val.selected }" @click="changeSelectedStatus(item, val)" v-if="val.picture" :src="val.picture" :title="val.name" />
+          <!-- 文字类型规格 -->
+          <span :class="{ selected: val.selected }" @click="changeSelectedStatus(item, val)" v-else>{{ val.name }}</span>
+        </template>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import axios from 'axios'
+  // 商品数据
+  const goods = ref({})
+  const getGoods = async () => {
+    // 1135076  初始化就有无库存的规格
+    // 1369155859933827074 更新之后有无库存项（蓝色-20cm-中国）
+    const res = await axios.get('http://pcapi-xiaotuxian-front-devtest.itheima.net/goods?id=1369155859933827074')
+    goods.value = res.data.result
+  }
+  onMounted(() => getGoods())
+
+  //切换选中状态
+  const changeSelectedStatus = (item, val) => {
+    //item：同一排对象
+    //val：当前点击项
+
+    if (val.selected) {
+      val.selected = false
+    } else {
+      //第一次为所有对象添加selected属性
+      //遍历同一排其他对象
+      item.values.forEach((element) => {
+        element.selected = false
+      })
+      val.selected = true
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @mixin sku-state-mixin {
+    border: 1px solid #e4e4e4;
+    margin-right: 10px;
+    cursor: pointer;
+
+    &.selected {
+      border-color: #27ba9b;
+    }
+
+    &.disabled {
+      opacity: 0.6;
+      border-style: dashed;
+      cursor: not-allowed;
+    }
+  }
+
+  .goods-sku {
+    padding-left: 10px;
+    padding-top: 20px;
+
+    dl {
+      display: flex;
+      padding-bottom: 20px;
+      align-items: center;
+
+      dt {
+        width: 50px;
+        color: #999;
+      }
+
+      dd {
+        flex: 1;
+        color: #666;
+
+        > img {
+          width: 50px;
+          height: 50px;
+          margin-bottom: 4px;
+          @include sku-state-mixin;
+        }
+
+        > span {
+          display: inline-block;
+          height: 30px;
+          line-height: 28px;
+          padding: 0 20px;
+          margin-bottom: 4px;
+          @include sku-state-mixin;
+        }
+      }
+    }
+  }
+</style>
